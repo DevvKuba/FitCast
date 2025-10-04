31da95c560b1fdf5e4a95293e955833da539a2d9
DevvKuba <jrfitnessinbox@gmail.com>
2025-10-04 22:13:29 +0100

    Create new login and register components

--- ClientDashboard_GUI/src/app/services/account.service.ts
+++ ClientDashboard_GUI/src/app/services/account.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ApiResponse } from '../models/api-response';
 import { Observable } from 'rxjs';
+import { RegisterDto } from '../models/register-dto';
+import { LoginDto } from '../models/login-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,11 @@ import { Observable } from 'rxjs';
 export class AccountService {
   http = inject(HttpClient);
 
-  register() : Observable<ApiResponse<any>>{
-    return this.http.post<ApiResponse<string>>("https://clientdashboardapp-dfdja3c4hxffdsg0.uksouth-01.azurewebsites.net/register")
+  register(registerInfo : RegisterDto) : Observable<ApiResponse<any>>{
+    return this.http.post<ApiResponse<string>>("https://clientdashboardapp-dfdja3c4hxffdsg0.uksouth-01.azurewebsites.net/register", registerInfo);
   }
 
-  login() : Observable<ApiResponse<any>>{
-    return this.http.get<ApiResponse<string>>("https://clientdashboardapp-dfdja3c4hxffdsg0.uksouth-01.azurewebsites.net/login");
+  login(loginInfo : LoginDto) : Observable<ApiResponse<any>>{
+    return this.http.post<ApiResponse<string>>("https://clientdashboardapp-dfdja3c4hxffdsg0.uksouth-01.azurewebsites.net/login", loginInfo);
   }
 }
