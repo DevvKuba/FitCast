bc63e36a4b872d8fd6e23620b2c04bd5a7eb921d
DevvKuba <jrfitnessinbox@gmail.com>
2025-10-05 18:05:36 +0100

    Finalise rough register page containing input field containing registerDto values

--- ClientDashboard_GUI/src/app/register/register.component.ts
+++ ClientDashboard_GUI/src/app/register/register.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { Message } from 'primeng/message';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Message, InputTextModule, PasswordModule, IftaLabelModule, FormsModule, FloatLabelModule, ButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  email: string = "";
+  firstName: string = "";
+  surname: string = "";
+  password: string = "";
 
+  trainerRegister(email: string, firstName: string, surname: string, password: string){
+
+  }
 }
