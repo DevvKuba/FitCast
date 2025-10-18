52ae35dcfe48f4fdec6eca696d122afdaf4893ba
kuba <kubaraczkiewicz1@gmail.com>
2025-10-18 22:53:36 +0100

    Include interceptor within providers app.config array

--- ClientDashboard_GUI/src/app/app.config.ts
+++ ClientDashboard_GUI/src/app/app.config.ts
@@ -4,13 +4,15 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MessageService } from 'primeng/api';
+import { jwtInterceptorInterceptor } from './interceptors/jwt-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([jwtInterceptorInterceptor])),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
