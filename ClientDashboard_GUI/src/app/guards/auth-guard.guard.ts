dedccafb0d515b26118521e87b7af06faef32682
DevvKuba <jrfitnessinbox@gmail.com>
2025-10-22 14:52:29 +0100

    Add createUrlTree return type within guard to redirect user back to default home page

--- ClientDashboard_GUI/src/app/guards/auth-guard.guard.ts
+++ ClientDashboard_GUI/src/app/guards/auth-guard.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router} from '@angular/router';
 import { AccountService } from '../services/account.service';
 import { inject } from '@angular/core';
 import { ToastService } from '../services/toast.service';
@@ -6,11 +6,13 @@ import { ToastService } from '../services/toast.service';
 export const authGuard: CanActivateFn = () => {
   const accountService = inject(AccountService);
   const toast = inject(ToastService);
+  const router = inject(Router);
+
   if(accountService.currentUser()?.token){
     return true;
   }
   toast.toastSummary = "Unauthorized";
   toast.toastDetail = "You must log in before accessing this page";
   toast.showError();
-  return false;
+  return router.createUrlTree(['']);
 };
