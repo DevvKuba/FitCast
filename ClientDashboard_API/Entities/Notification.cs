d4e4bb2116ac68716744ed61b6b97819091a02c7
DevvKuba <jrfitnessinbox@gmail.com>
2025-11-02 23:44:15 +0000

    Declare new Notification entity

---
+++ ClientDashboard_API/Entities/Notification.cs
@@ -0,0 +1,19 @@
+﻿namespace ClientDashboard_API.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        public int TrainerId { get; set; }
+
+        public int? ClientId { get; set; }
+
+        public required string Message { get; set; }
+
+        public required string ReminderType { get; set; }
+
+        public required string SentThrough { get; set; } // email , sms.. 
+
+        public DateTime SentAt { get; set; }
+    }
+}
