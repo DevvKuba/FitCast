7bd69b96055d455c07e2b3a9a27c0a1d10a567cd
DevvKuba <jrfitnessinbox@gmail.com>
2025-11-03 21:43:39 +0000

    Implement both Add and Delete methods within NotificationRepository

--- ClientDashboard_API/Data/Migrations/NotificationRepository.cs
+++ ClientDashboard_API/Data/Migrations/NotificationRepository.cs
@@ -1,9 +1,27 @@
-﻿using ClientDashboard_API.Interfaces;
+﻿using ClientDashboard_API.Entities;
+using ClientDashboard_API.Interfaces;
 
 namespace ClientDashboard_API.Data.Migrations
 {
     public class NotificationRepository(DataContext context) : INotificationRepository
     {
+        public async Task AddNotificationAsync(int trainerId, int? clientId, string message, string reminderType, string sentThrough)
+        {
+            var newNotification = new Notification
+            {
+                TrainerId = trainerId,
+                ClientId = clientId ?? null,
+                Message = message,
+                ReminderType = reminderType,
+                SentThrough = sentThrough,
+                SentAt = DateTime.UtcNow
+            };
+            await context.Notification.AddAsync(newNotification);
+        }
 
+        public void DeleteNotification(Notification notification)
+        {
+            context.Notification.Remove(notification);
+        }
     }
 }
--- ClientDashboard_API/Interfaces/INotificationRepository.cs
+++ ClientDashboard_API/Interfaces/INotificationRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace ClientDashboard_API.Interfaces
+﻿using ClientDashboard_API.Entities;
+
+namespace ClientDashboard_API.Interfaces
 {
     public interface INotificationRepository
     {
+        void DeleteNotification(Notification notification);
+
+        Task AddNotificationAsync(int trainerId, int? clientId, string message, string reminderType, string sentThrough);
     }
 }
