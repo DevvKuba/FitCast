eb7cbd7a23c4222e5d306dd7bda8868a87e822d3
DevvKuba <jrfitnessinbox@gmail.com>
2025-11-05 19:21:05 +0000

    Create new Payments entity

---
+++ ClientDashboard_API/Entities/Payment.cs
@@ -0,0 +1,25 @@
+﻿namespace ClientDashboard_API.Entities
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public required string Currency { get; set; }
+
+        public required int NumberOfSessions { get; set; }
+
+        public required DateOnly PaymentDate { get; set; }
+
+        public int? TrainerId { get; set; }
+
+        public int? ClientId { get; set; }
+
+        public Trainer? Trainer { get; set; } = null!;
+
+        public Client? Client { get; set; } = null!;
+
+
+    }
+}
