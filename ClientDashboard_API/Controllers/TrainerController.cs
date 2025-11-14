5410efe571c0d4e926be30341f3b5b056505baf4
DevvKuba <jrfitnessinbox@gmail.com>
2025-11-14 17:46:00 +0000

    Create HttpPut request to gather Hevy workout sessions, upon calling valid trainer api key

--- ClientDashboard_API/Controllers/TrainerController.cs
+++ ClientDashboard_API/Controllers/TrainerController.cs
@@ -3,11 +3,12 @@ using ClientDashboard_API.DTOs;
 using ClientDashboard_API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Twilio.TwiML.Voice;
 
 namespace ClientDashboard_API.Controllers
 {
     [Authorize]
-    public class TrainerController(IUnitOfWork unitOfWork, IMapper mapper, IApiKeyEncryter encrypter, ISessionDataParser hevyDataParser) : BaseAPIController
+    public class TrainerController(IUnitOfWork unitOfWork, IApiKeyEncryter encrypter, ISessionDataParser hevyDataParser, ISessionSyncService syncService) : BaseAPIController
     {
         /// <summary>
         /// Trainer method allowing assignment of client under them
@@ -89,7 +90,28 @@ namespace ClientDashboard_API.Controllers
             {
                 return BadRequest(new ApiResponseDto<string> { Data = null, Message = $"error saving {trainer.FirstName}'s new api key", Success = false });
             }
-            return Ok(new ApiResponseDto<string> { Data = trainer.FirstName, Message = $"trainer: {trainer.FirstName}'s api ket successfully set up", Success = true });
+            return Ok(new ApiResponseDto<string> { Data = trainer.FirstName, Message = $"trainer: {trainer.FirstName}'s api key successfully set up", Success = true });
+        }
+
+        //summary>
+        /// Trainer method to collect daily client workout's from Hevy Workout Tracker
+        /// </summary>
+        [HttpPut("getDailyHevyWorkouts")]
+        public async Task<ActionResult<ApiResponseDto<string>>> GatherAndUpdateHevyClientWorkoutsAsync([FromQuery] int trainerId)
+        {
+            var trainer = await unitOfWork.TrainerRepository.GetTrainerByIdAsync(trainerId);
+
+            if (trainer == null)
+            {
+                return BadRequest(new ApiResponseDto<string> { Data = null, Message = "trainer does not exist", Success = false });
+            }
+
+            var result = await syncService.SyncSessionsAsync(trainerId);
+            if (!result)
+            {
+                return BadRequest(new ApiResponseDto<string> { Data = null, Message = "Hevy workout sync unsuccessful", Success = false });
+            }
+            return Ok(new ApiResponseDto<string> { Data = trainer.FirstName, Message = "Hevy workout sync successfully completed", Success = true });
         }
 
     }
