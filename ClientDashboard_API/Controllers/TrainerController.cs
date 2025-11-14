542c014d3e1d4afc91439423bed4ab38eb7cd5ea
DevvKuba <jrfitnessinbox@gmail.com>
2025-11-14 13:35:23 +0000

    Finalise TrainerController UpdateWorkoutRetrievalApiKeyAsync http method, allowing encapturing of specific trainer, verifying their api Key with a mock request then proceeding to it's setting and saving

--- ClientDashboard_API/Controllers/TrainerController.cs
+++ ClientDashboard_API/Controllers/TrainerController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ClientDashboard_API.Controllers
 {
     [Authorize]
-    public class TrainerController(IUnitOfWork unitOfWork, IMapper mapper, IApiKeyEncryter encrypter) : BaseAPIController
+    public class TrainerController(IUnitOfWork unitOfWork, IMapper mapper, IApiKeyEncryter encrypter, ISessionDataParser hevyDataParser) : BaseAPIController
     {
         /// <summary>
         /// Trainer method allowing assignment of client under them
@@ -66,19 +66,30 @@ namespace ClientDashboard_API.Controllers
         [HttpPut("updateTrainerApiKey")]
         public async Task<ActionResult<ApiResponseDto<string>>> UpdateWorkoutRetrievalApiKeyAsync([FromQuery] int trainerId, [FromQuery] string providedApiKey)
         {
-            // check for trainer
+            var trainer = await unitOfWork.TrainerRepository.GetTrainerByIdAsync(trainerId);
 
+            if (trainer == null)
+            {
+                return BadRequest(new ApiResponseDto<string> { Data = null, Message = "trainer does not exist", Success = false });
+            }
             // have a dummy / test method within HevySessionDataService can uses the apiKey to try and get a 200 response 
 
+            if (!await hevyDataParser.IsApiKeyValidAsync(providedApiKey))
+            {
+                return BadRequest(new ApiResponseDto<string> { Data = null, Message = $"provided api key: {providedApiKey} is not valid.", Success = false });
+            }
+
             // if that's the case use encryption service to encrypt the functioning key
+            var encryptedApiKey = encrypter.Encrypt(providedApiKey);
 
             // store for trainer RetrievalWorkoutApiKey property
+            await unitOfWork.TrainerRepository.UpdateTrainerApiKeyAsync(trainerId, encryptedApiKey);
 
-            // else BadRequest around apiKey not being valid
-
-            // unitOfWork.Complete() etc..
-
-            throw new NotImplementedException();
+            if (!await unitOfWork.Complete())
+            {
+                return BadRequest(new ApiResponseDto<string> { Data = null, Message = $"error saving {trainer.FirstName}'s new api key", Success = false });
+            }
+            return Ok(new ApiResponseDto<string> { Data = trainer.FirstName, Message = $"trainer: {trainer.FirstName}'s api ket successfully set up", Success = true });
         }
 
     }
