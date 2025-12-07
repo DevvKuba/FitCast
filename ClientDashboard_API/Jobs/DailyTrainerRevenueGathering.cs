a9a06df8b0fbbf6f06bbb48f5aebc893cf5506ed
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-07 19:12:25 +0000

    Complete DailyTrainerRevenueGathering background Quartz job

---
+++ ClientDashboard_API/Jobs/DailyTrainerRevenueGathering.cs
@@ -0,0 +1,27 @@
+﻿using ClientDashboard_API.Entities;
+using ClientDashboard_API.Interfaces;
+using Quartz;
+
+namespace ClientDashboard_API.Jobs
+{
+    public class DailyTrainerRevenueGathering(IUnitOfWork unitOfWork, ITrainerDailyRevenueService revenueService, ILogger<DailyTrainerRevenueGathering> logger) : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            logger.LogInformation("DailyTrainerRevenueGathering process is beginning at: {DateToday}", DateOnly.FromDateTime(DateTime.UtcNow));
+
+            var eligibleTrainers = await unitOfWork.TrainerRepository.GetAllTrainersEligibleForRevenueTrackingAsync();
+
+            logger.LogDebug("Gathered: {TrainerCount} eligible trainers for revenue data gathering", eligibleTrainers.Count);
+
+            foreach(Trainer trainer in eligibleTrainers)
+            {
+                await revenueService.ExecuteTrainerDailyRevenueGatheringAsync(trainer);
+
+                logger.LogDebug("Processed trainer: {TrainerName}'s revenue at: {DateToday}", trainer.FirstName, DateOnly.FromDateTime(DateTime.UtcNow));
+            }
+
+            logger.LogInformation("DailyTrainerRevenueGathering process has finalised at: {DateToday}", DateOnly.FromDateTime(DateTime.UtcNow));
+        }
+    }
+}
