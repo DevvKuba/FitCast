ab79892d4982432f6534586c7b4d45c2b734d7d8
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-18 13:33:42 +0000

    Generate and apply migrations ensuring each Trainer user is initially created with the EmailVerified property flagged as false

---
+++ ClientDashboard_API/Data/Migrations/20251218133232_AddEmailVerifiedTrainerProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientDashboard_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerifiedTrainerProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailVerified",
+                table: "Trainers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerified",
+                table: "Trainers");
+        }
+    }
+}
