dc06aeecad0b2ee514a2a3b331c5998b00374755
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-18 12:08:40 +0000

    Add and apply migrations including the newly declared entity

---
+++ ClientDashboard_API/Data/Migrations/20251218120611_AddEmailVerificationTokenEntity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientDashboard_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationTokenEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_Clients_ClientId",
+                table: "Notification");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_Trainers_TrainerId",
+                table: "Notification");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification");
+
+            migrationBuilder.RenameTable(
+                name: "Notification",
+                newName: "Notifications");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_TrainerId",
+                table: "Notifications",
+                newName: "IX_Notifications_TrainerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_ClientId",
+                table: "Notifications",
+                newName: "IX_Notifications_ClientId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "EmailVerificationTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TrainerId = table.Column<int>(type: "int", nullable: false),
+                    CreatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpiresOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailVerificationTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailVerificationTokens_Trainers_TrainerId",
+                        column: x => x.TrainerId,
+                        principalTable: "Trainers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailVerificationTokens_TrainerId",
+                table: "EmailVerificationTokens",
+                column: "TrainerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Clients_ClientId",
+                table: "Notifications",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Trainers_TrainerId",
+                table: "Notifications",
+                column: "TrainerId",
+                principalTable: "Trainers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Clients_ClientId",
+                table: "Notifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Trainers_TrainerId",
+                table: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "EmailVerificationTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications");
+
+            migrationBuilder.RenameTable(
+                name: "Notifications",
+                newName: "Notification");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_TrainerId",
+                table: "Notification",
+                newName: "IX_Notification_TrainerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_ClientId",
+                table: "Notification",
+                newName: "IX_Notification_ClientId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_Clients_ClientId",
+                table: "Notification",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_Trainers_TrainerId",
+                table: "Notification",
+                column: "TrainerId",
+                principalTable: "Trainers",
+                principalColumn: "Id");
+        }
+    }
+}
