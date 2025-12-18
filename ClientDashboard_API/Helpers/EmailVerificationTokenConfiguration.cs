e3366cdf0b0ca6411aad911ef68e681512147f52
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-18 12:36:18 +0000

    Provide explicit EmailVerificationTokenConfiguration

---
+++ ClientDashboard_API/Helpers/EmailVerificationTokenConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ClientDashboard_API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ClientDashboard_API.Helpers
+{
+    internal sealed class EmailVerificationTokenConfiguration : IEntityTypeConfiguration<EmailVerificationToken>
+    {
+        public void Configure(EntityTypeBuilder<EmailVerificationToken> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            // use-case for re-sending token
+            // where trainer has multiple assigned verification tokens
+            builder.HasOne(e => e.Trainer).WithMany().HasForeignKey(e => e.TrainerId);
+        }
+    }
+}
