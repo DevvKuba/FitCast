7955fa8e5e4f91489e321912f4120220a291edd6
kuba <jrfitnessinbox@gmail.com>
2025-12-21 21:57:00 +0000

    Implement further unit tests to ensure all WorkoutRepository methods cases are covered

--- ProjectTests/ClientDashboard_API_Tests/RepositoryTests/WorkoutRepositoryTests.cs
+++ ProjectTests/ClientDashboard_API_Tests/RepositoryTests/WorkoutRepositoryTests.cs
@@ -218,6 +218,375 @@ namespace ClientDashboard_API_Tests.RepositoryTests
             Assert.True(workouts!.Any(x => x.WorkoutTitle == "workout 1"));
             Assert.True(workouts!.Any(x => x.WorkoutTitle == "workout 3"));
         }
+
+        [Fact]
+        public async Task TestGetSpecificClientsWorkoutsAsync()
+        {
+            var client1 = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            var client2 = new Client 
+            { 
+                Role = "client", 
+                FirstName = "mark", 
+                CurrentBlockSession = 2, 
+                TotalBlockSessions = 8, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client1);
+            await _context.Client.AddAsync(client2);
+            await _unitOfWork.Complete();
+
+            await _context.Workouts.AddAsync(new Workout
+            {
+                ClientId = client1.Id,
+                ClientName = "rob",
+                WorkoutTitle = "workout 1",
+                SessionDate = DateOnly.Parse("19/06/2024"),
+                Client = client1
+            });
+            await _context.Workouts.AddAsync(new Workout
+            {
+                ClientId = client2.Id,
+                ClientName = "mark",
+                WorkoutTitle = "workout 2",
+                SessionDate = DateOnly.Parse("20/06/2024"),
+                Client = client2
+            });
+            await _unitOfWork.Complete();
+
+            var clients = await _context.Client.Include(c => c.Workouts).ToListAsync();
+            var workouts = _workoutRepository.GetSpecificClientsWorkoutsAsync(clients);
+
+            Assert.Equal(2, workouts.Count);
+            Assert.Contains(workouts, w => w.WorkoutTitle == "workout 1");
+            Assert.Contains(workouts, w => w.WorkoutTitle == "workout 2");
+            Assert.All(workouts, w => Assert.Null(w.Client));
+            Assert.Equal("workout 2", workouts[0].WorkoutTitle);
+        }
+
+        [Fact]
+        public async Task TestGetWorkoutByIdAsync()
+        {
+            var workout = new Workout
+            {
+                ClientName = "rob",
+                WorkoutTitle = "test workout",
+                SessionDate = DateOnly.Parse("19/06/2024")
+            };
+            await _context.Workouts.AddAsync(workout);
+            await _unitOfWork.Complete();
+
+            var retrievedWorkout = await _workoutRepository.GetWorkoutByIdAsync(workout.Id);
+
+            Assert.NotNull(retrievedWorkout);
+            Assert.Equal(workout.Id, retrievedWorkout.Id);
+            Assert.Equal("test workout", retrievedWorkout.WorkoutTitle);
+        }
+
+        [Fact]
+        public async Task TestGetWorkoutByIdReturnsNullForNonExistentIdAsync()
+        {
+            var workout = await _workoutRepository.GetWorkoutByIdAsync(999);
+
+            Assert.Null(workout);
+        }
+
+        [Fact]
+        public async Task TestGetClientWorkoutsAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            await _context.Workouts.AddAsync(new Workout
+            {
+                ClientId = client.Id,
+                ClientName = "rob",
+                WorkoutTitle = "workout 1",
+                SessionDate = DateOnly.Parse("19/06/2024"),
+                Client = client
+            });
+            await _context.Workouts.AddAsync(new Workout
+            {
+                ClientId = client.Id,
+                ClientName = "rob",
+                WorkoutTitle = "workout 2",
+                SessionDate = DateOnly.Parse("20/06/2024"),
+                Client = client
+            });
+            await _unitOfWork.Complete();
+
+            var workouts = await _workoutRepository.GetClientWorkoutsAsync(client);
+
+            Assert.Equal(2, workouts.Count);
+            Assert.All(workouts, w => Assert.Null(w.Client));
+            Assert.Equal("workout 2", workouts[0].WorkoutTitle);
+            Assert.Equal("workout 1", workouts[1].WorkoutTitle);
+        }
+
+        [Fact]
+        public async Task TestGetClientWorkoutAtDateByIdAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            var testWorkout = new Workout
+            {
+                ClientId = client.Id,
+                ClientName = "rob",
+                WorkoutTitle = "workout 1",
+                SessionDate = DateOnly.Parse("19/06/2024")
+            };
+            await _context.Workouts.AddAsync(testWorkout);
+            await _unitOfWork.Complete();
+
+            var workout = await _workoutRepository.GetClientWorkoutAtDateByIdAsync(client.Id, DateOnly.Parse("19/06/2024"));
+
+            Assert.NotNull(workout);
+            Assert.Equal(testWorkout.Id, workout.Id);
+            Assert.Equal("workout 1", workout.WorkoutTitle);
+        }
+
+        [Fact]
+        public async Task TestGetClientWorkoutAtDateByIdReturnsNullForDifferentDateAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            await _context.Workouts.AddAsync(new Workout
+            {
+                ClientId = client.Id,
+                ClientName = "rob",
+                WorkoutTitle = "workout 1",
+                SessionDate = DateOnly.Parse("19/06/2024")
+            });
+            await _unitOfWork.Complete();
+
+            var workout = await _workoutRepository.GetClientWorkoutAtDateByIdAsync(client.Id, DateOnly.Parse("20/06/2024"));
+
+            Assert.Null(workout);
+        }
+
+        [Fact]
+        public async Task TestGetSessionCountAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("15/06/2024") });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("17/06/2024") });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("19/06/2024") });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("25/06/2024") });
+            await _unitOfWork.Complete();
+
+            var count = await _workoutRepository.GetSessionCountAsync(client, DateOnly.Parse("16/06/2024"), DateOnly.Parse("20/06/2024"));
+
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public async Task TestGetSessionCountLast7DaysAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            var untilDate = DateOnly.Parse("20/06/2024");
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("10/06/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("15/06/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("18/06/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("20/06/2024") });
+            await _unitOfWork.Complete();
+
+            var count = await _workoutRepository.GetSessionCountLast7DaysAsync(client, untilDate);
+
+            Assert.Equal(3, count);
+        }
+
+        [Fact]
+        public async Task TestGetSessionCountLast28DaysAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            var untilDate = DateOnly.Parse("20/06/2024");
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("20/05/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("25/05/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("10/06/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("15/06/2024") });
+            await _context.Workouts.AddAsync(new Workout {WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("20/06/2024") });
+            await _unitOfWork.Complete();
+
+            var count = await _workoutRepository.GetSessionCountLast28DaysAsync(client, untilDate);
+
+            Assert.Equal(4, count);
+        }
+
+        [Fact]
+        public async Task TestGetDaysFromLastSessionAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("15/06/2024") });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("18/06/2024") });
+            await _unitOfWork.Complete();
+
+            var untilDate = DateOnly.Parse("25/06/2024");
+            var days = await _workoutRepository.GetDaysFromLastSessionAsync(client, untilDate);
+
+            Assert.NotNull(days);
+            Assert.Equal(7, days);
+        }
+
+        [Fact]
+        public async Task TestGetDaysFromLastSessionReturnsNullWhenNoWorkoutsAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            var untilDate = DateOnly.Parse("25/06/2024");
+            var days = await _workoutRepository.GetDaysFromLastSessionAsync(client, untilDate);
+
+            Assert.Null(days);
+        }
+
+        [Fact]
+        public async Task TestUpdateWorkoutAsync()
+        {
+            var workout = new Workout
+            {
+                ClientName = "rob",
+                WorkoutTitle = "old title",
+                SessionDate = DateOnly.Parse("19/06/2024"),
+                ExerciseCount = 5,
+                Duration = 45
+            };
+            await _context.Workouts.AddAsync(workout);
+            await _unitOfWork.Complete();
+
+            _workoutRepository.UpdateWorkout(workout, "new title", DateOnly.Parse("20/06/2024"), 10, 60);
+            await _unitOfWork.Complete();
+
+            Assert.Equal("new title", workout.WorkoutTitle);
+            Assert.Equal(DateOnly.Parse("20/06/2024"), workout.SessionDate);
+            Assert.Equal(10, workout.ExerciseCount);
+            Assert.Equal(60, workout.Duration);
+        }
+
+        [Fact]
+        public async Task TestCalculateClientMeanWorkoutDurationAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 1", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("15/06/2024"), Duration = 40 });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 2", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("17/06/2024"), Duration = 50 });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 3" , ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("19/06/2024"), Duration = 60 });
+            await _context.Workouts.AddAsync(new Workout { WorkoutTitle = "workout 4", ClientName = "rob", ClientId = client.Id, SessionDate = DateOnly.Parse("25/06/2024"), Duration = 70 });
+            await _unitOfWork.Complete();
+
+            var tillDate = DateOnly.Parse("20/06/2024");
+            var meanDuration = await _workoutRepository.CalculateClientMeanWorkoutDurationAsync(client, tillDate);
+
+            Assert.Equal(50, meanDuration);
+        }
+
+        [Fact]
+        public async Task TestCalculateClientMeanWorkoutDurationReturnsZeroWhenNoWorkoutsAsync()
+        {
+            var client = new Client 
+            { 
+                Role = "client", 
+                FirstName = "rob", 
+                CurrentBlockSession = 1, 
+                TotalBlockSessions = 4, 
+                Workouts = [] 
+            };
+            await _context.Client.AddAsync(client);
+            await _unitOfWork.Complete();
+
+            var tillDate = DateOnly.Parse("20/06/2024");
+            var meanDuration = await _workoutRepository.CalculateClientMeanWorkoutDurationAsync(client, tillDate);
+
+            Assert.Equal(0, meanDuration);
+        }
     }
 }
 
