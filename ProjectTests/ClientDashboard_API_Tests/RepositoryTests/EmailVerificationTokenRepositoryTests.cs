19c0d95a66e5944c88f403076fa8d3b224eb8e01
kuba <jrfitnessinbox@gmail.com>
2025-12-23 11:25:19 +0000

    Finish remaining unit tests for TrainerDailyRevenue & EmailVerificationToken repositories

--- ProjectTests/ClientDashboard_API_Tests/RepositoryTests/EmailVerificationTokenRepositoryTests.cs
+++ ProjectTests/ClientDashboard_API_Tests/RepositoryTests/EmailVerificationTokenRepositoryTests.cs
@@ -1,13 +1,161 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AutoMapper;
+using ClientDashboard_API.Data;
+using ClientDashboard_API.Dto_s;
+using ClientDashboard_API.DTOs;
+using ClientDashboard_API.Entities;
+using ClientDashboard_API.Helpers;
+using ClientDashboard_API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ClientDashboard_API_Tests.RepositoryTests
 {
-    internal class EmailVerificationTokenRepositoryTests
+    public class EmailVerificationTokenRepositoryTests
     {
-        // Test methods will be implemented here in the future.
+        private readonly IMapper _mapper;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly DataContext _context;
+        private readonly UserRepository _userRepository;
+        private readonly ClientRepository _clientRepository;
+        private readonly WorkoutRepository _workoutRepository;
+        private readonly TrainerRepository _trainerRepository;
+        private readonly NotificationRepository _notificationRepository;
+        private readonly PaymentRepository _paymentRepository;
+        private readonly EmailVerificationTokenRepository _emailVerificationTokenRepository;
+        private readonly ClientDailyFeatureRepository _clientDailyFeatureRepository;
+        private readonly TrainerDailyRevenueRepository _trainerDailyRevenueRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public EmailVerificationTokenRepositoryTests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Client, WorkoutDto>();
+                cfg.CreateMap<ClientUpdateDto, Client>();
+                cfg.CreateMap<PaymentUpdateDto, Payment>();
+                cfg.CreateMap<TrainerUpdateDto, Trainer>();
+            });
+            _mapper = config.CreateMapper();
+            _passwordHasher = new PasswordHasher();
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            _context = new DataContext(optionsBuilder.Options);
+            _userRepository = new UserRepository(_context);
+            _clientRepository = new ClientRepository(_context, _passwordHasher, _mapper);
+            _workoutRepository = new WorkoutRepository(_context);
+            _trainerRepository = new TrainerRepository(_context, _mapper);
+            _notificationRepository = new NotificationRepository(_context);
+            _paymentRepository = new PaymentRepository(_context, _mapper);
+            _emailVerificationTokenRepository = new EmailVerificationTokenRepository(_context);
+            _clientDailyFeatureRepository = new ClientDailyFeatureRepository(_context);
+            _trainerDailyRevenueRepository = new TrainerDailyRevenueRepository(_context);
+            _unitOfWork = new UnitOfWork(_context, _userRepository, _clientRepository, _workoutRepository, _trainerRepository, _notificationRepository, _paymentRepository, _emailVerificationTokenRepository, _clientDailyFeatureRepository, _trainerDailyRevenueRepository);
+        }
+
+        [Fact]
+        public async Task TestAddEmailVerificationTokenAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var token = new EmailVerificationToken
+            {
+                TrainerId = trainer.Id,
+                CreatedOnUtc = DateTime.UtcNow,
+                ExpiresOnUtc = DateTime.UtcNow.AddHours(24)
+            };
+
+            await _emailVerificationTokenRepository.AddEmailVerificationTokenAsync(token);
+            await _unitOfWork.Complete();
+
+            var savedToken = await _context.EmailVerificationToken.FirstOrDefaultAsync();
+
+            Assert.NotNull(savedToken);
+            Assert.Equal(trainer.Id, savedToken.TrainerId);
+            Assert.True(savedToken.CreatedOnUtc <= DateTime.UtcNow);
+            Assert.True(savedToken.ExpiresOnUtc > DateTime.UtcNow);
+        }
+
+        [Fact]
+        public async Task TestGetEmailVerificationTokenByIdAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var token = new EmailVerificationToken
+            {
+                TrainerId = trainer.Id,
+                CreatedOnUtc = DateTime.UtcNow,
+                ExpiresOnUtc = DateTime.UtcNow.AddHours(24)
+            };
+            await _context.EmailVerificationToken.AddAsync(token);
+            await _unitOfWork.Complete();
+
+            var retrievedToken = await _emailVerificationTokenRepository.GetEmailVerificationTokenByIdAsync(token.Id);
+
+            Assert.NotNull(retrievedToken);
+            Assert.Equal(token.Id, retrievedToken.Id);
+            Assert.Equal(trainer.Id, retrievedToken.TrainerId);
+        }
+
+        [Fact]
+        public async Task TestGetEmailVerificationTokenByIdReturnsNullForNonExistentIdAsync()
+        {
+            var token = await _emailVerificationTokenRepository.GetEmailVerificationTokenByIdAsync(999);
+
+            Assert.Null(token);
+        }
+
+        [Fact]
+        public async Task TestAddEmailVerificationTokenWithExpirationAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var createdTime = DateTime.UtcNow;
+            var expirationTime = createdTime.AddHours(48);
+
+            var token = new EmailVerificationToken
+            {
+                TrainerId = trainer.Id,
+                CreatedOnUtc = createdTime,
+                ExpiresOnUtc = expirationTime
+            };
+
+            await _emailVerificationTokenRepository.AddEmailVerificationTokenAsync(token);
+            await _unitOfWork.Complete();
+
+            var savedToken = await _context.EmailVerificationToken.FirstOrDefaultAsync();
+
+            Assert.NotNull(savedToken);
+            Assert.Equal(trainer.Id, savedToken.TrainerId);
+            Assert.Equal(createdTime.Date, savedToken.CreatedOnUtc.Date);
+            Assert.Equal(expirationTime.Date, savedToken.ExpiresOnUtc.Date);
+        }
     }
 }
--- ProjectTests/ClientDashboard_API_Tests/RepositoryTests/TrainerDailyRevenueRepositoryTests.cs
+++ ProjectTests/ClientDashboard_API_Tests/RepositoryTests/TrainerDailyRevenueRepositoryTests.cs
@@ -1,12 +1,224 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AutoMapper;
+using ClientDashboard_API.Data;
+using ClientDashboard_API.Dto_s;
+using ClientDashboard_API.DTOs;
+using ClientDashboard_API.Entities;
+using ClientDashboard_API.Helpers;
+using ClientDashboard_API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ClientDashboard_API_Tests.RepositoryTests
 {
-    internal class TrainerDailyRevenueRepositoryTests
+    public class TrainerDailyRevenueRepositoryTests
     {
+        private readonly IMapper _mapper;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly DataContext _context;
+        private readonly UserRepository _userRepository;
+        private readonly ClientRepository _clientRepository;
+        private readonly WorkoutRepository _workoutRepository;
+        private readonly TrainerRepository _trainerRepository;
+        private readonly NotificationRepository _notificationRepository;
+        private readonly PaymentRepository _paymentRepository;
+        private readonly EmailVerificationTokenRepository _emailVerificationTokenRepository;
+        private readonly ClientDailyFeatureRepository _clientDailyFeatureRepository;
+        private readonly TrainerDailyRevenueRepository _trainerDailyRevenueRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public TrainerDailyRevenueRepositoryTests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Client, WorkoutDto>();
+                cfg.CreateMap<ClientUpdateDto, Client>();
+                cfg.CreateMap<PaymentUpdateDto, Payment>();
+                cfg.CreateMap<TrainerUpdateDto, Trainer>();
+            });
+            _mapper = config.CreateMapper();
+            _passwordHasher = new PasswordHasher();
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            _context = new DataContext(optionsBuilder.Options);
+            _userRepository = new UserRepository(_context);
+            _clientRepository = new ClientRepository(_context, _passwordHasher, _mapper);
+            _workoutRepository = new WorkoutRepository(_context);
+            _trainerRepository = new TrainerRepository(_context, _mapper);
+            _notificationRepository = new NotificationRepository(_context);
+            _paymentRepository = new PaymentRepository(_context, _mapper);
+            _emailVerificationTokenRepository = new EmailVerificationTokenRepository(_context);
+            _clientDailyFeatureRepository = new ClientDailyFeatureRepository(_context);
+            _trainerDailyRevenueRepository = new TrainerDailyRevenueRepository(_context);
+            _unitOfWork = new UnitOfWork(_context, _userRepository, _clientRepository, _workoutRepository, _trainerRepository, _notificationRepository, _paymentRepository, _emailVerificationTokenRepository, _clientDailyFeatureRepository, _trainerDailyRevenueRepository);
+        }
+
+        [Fact]
+        public async Task TestAddTrainerDailyRevenueRecordAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var trainerDailyData = new TrainerDailyDataAddDto
+            {
+                TrainerId = trainer.Id,
+                RevenueToday = 250.00m,
+                MonthlyRevenueThusFar = 3500.00m,
+                TotalSessionsThisMonth = 45,
+                NewClientsThisMonth = 3,
+                ActiveClients = 12,
+                AverageSessionPrice = 75.00m,
+                AsOfDate = DateOnly.Parse("15/06/2025")
+            };
+
+            await _trainerDailyRevenueRepository.AddTrainerDailyRevenueRecordAsync(trainerDailyData);
+            await _unitOfWork.Complete();
+
+            var savedRecord = await _context.TrainerDailyRevenue.FirstOrDefaultAsync();
+
+            Assert.NotNull(savedRecord);
+            Assert.Equal(trainer.Id, savedRecord.TrainerId);
+            Assert.Equal(250.00m, savedRecord.RevenueToday);
+            Assert.Equal(3500.00m, savedRecord.MonthlyRevenueThusFar);
+            Assert.Equal(45, savedRecord.TotalSessionsThisMonth);
+            Assert.Equal(3, savedRecord.NewClientsThisMonth);
+            Assert.Equal(12, savedRecord.ActiveClients);
+            Assert.Equal(75.00m, savedRecord.AverageSessionPrice);
+            Assert.Equal(DateOnly.Parse("15/06/2025"), savedRecord.AsOfDate);
+        }
+
+        [Fact]
+        public async Task TestAddTrainerDailyRevenueRecordWithZeroRevenueAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var trainerDailyData = new TrainerDailyDataAddDto
+            {
+                TrainerId = trainer.Id,
+                RevenueToday = 0m,
+                MonthlyRevenueThusFar = 1000.00m,
+                TotalSessionsThisMonth = 10,
+                NewClientsThisMonth = 0,
+                ActiveClients = 5,
+                AverageSessionPrice = 50.00m,
+                AsOfDate = DateOnly.Parse("01/06/2025")
+            };
+
+            await _trainerDailyRevenueRepository.AddTrainerDailyRevenueRecordAsync(trainerDailyData);
+            await _unitOfWork.Complete();
+
+            var savedRecord = await _context.TrainerDailyRevenue.FirstOrDefaultAsync();
+
+            Assert.NotNull(savedRecord);
+            Assert.Equal(0m, savedRecord.RevenueToday);
+            Assert.Equal(0, savedRecord.NewClientsThisMonth);
+            Assert.Equal(1000.00m, savedRecord.MonthlyRevenueThusFar);
+        }
+
+        [Fact]
+        public async Task TestAddMultipleTrainerDailyRevenueRecordsAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var record1 = new TrainerDailyDataAddDto
+            {
+                TrainerId = trainer.Id,
+                RevenueToday = 100.00m,
+                MonthlyRevenueThusFar = 500.00m,
+                TotalSessionsThisMonth = 10,
+                NewClientsThisMonth = 1,
+                ActiveClients = 5,
+                AverageSessionPrice = 50.00m,
+                AsOfDate = DateOnly.Parse("01/06/2025")
+            };
+
+            var record2 = new TrainerDailyDataAddDto
+            {
+                TrainerId = trainer.Id,
+                RevenueToday = 150.00m,
+                MonthlyRevenueThusFar = 650.00m,
+                TotalSessionsThisMonth = 13,
+                NewClientsThisMonth = 1,
+                ActiveClients = 5,
+                AverageSessionPrice = 50.00m,
+                AsOfDate = DateOnly.Parse("02/06/2025")
+            };
+
+            await _trainerDailyRevenueRepository.AddTrainerDailyRevenueRecordAsync(record1);
+            await _trainerDailyRevenueRepository.AddTrainerDailyRevenueRecordAsync(record2);
+            await _unitOfWork.Complete();
+
+            var savedRecords = await _context.TrainerDailyRevenue.ToListAsync();
+
+            Assert.Equal(2, savedRecords.Count);
+            Assert.Contains(savedRecords, r => r.AsOfDate == DateOnly.Parse("01/06/2025"));
+            Assert.Contains(savedRecords, r => r.AsOfDate == DateOnly.Parse("02/06/2025"));
+            Assert.All(savedRecords, r => Assert.Equal(trainer.Id, r.TrainerId));
+        }
+
+        [Fact]
+        public async Task TestAddTrainerDailyRevenueRecordWithHighRevenueAsync()
+        {
+            var trainer = new Trainer
+            {
+                FirstName = "john",
+                Surname = "doe",
+                Email = "john@example.com",
+                Role = "trainer"
+            };
+            await _context.Trainer.AddAsync(trainer);
+            await _unitOfWork.Complete();
+
+            var trainerDailyData = new TrainerDailyDataAddDto
+            {
+                TrainerId = trainer.Id,
+                RevenueToday = 1500.00m,
+                MonthlyRevenueThusFar = 25000.00m,
+                TotalSessionsThisMonth = 200,
+                NewClientsThisMonth = 15,
+                ActiveClients = 50,
+                AverageSessionPrice = 125.00m,
+                AsOfDate = DateOnly.Parse("30/06/2025")
+            };
+
+            await _trainerDailyRevenueRepository.AddTrainerDailyRevenueRecordAsync(trainerDailyData);
+            await _unitOfWork.Complete();
+
+            var savedRecord = await _context.TrainerDailyRevenue.FirstOrDefaultAsync();
+
+            Assert.NotNull(savedRecord);
+            Assert.Equal(1500.00m, savedRecord.RevenueToday);
+            Assert.Equal(25000.00m, savedRecord.MonthlyRevenueThusFar);
+            Assert.Equal(200, savedRecord.TotalSessionsThisMonth);
+            Assert.Equal(15, savedRecord.NewClientsThisMonth);
+            Assert.Equal(50, savedRecord.ActiveClients);
+            Assert.Equal(125.00m, savedRecord.AverageSessionPrice);
+        }
     }
 }
