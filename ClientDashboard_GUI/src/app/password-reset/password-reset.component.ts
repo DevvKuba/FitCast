c70e3fb27daaf0ae282ba96d6ed2410bafd64067
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-30 19:50:10 +0000

    Provide appropriate input fields and button within password reset page, along with setting up routing back to home page

--- ClientDashboard_GUI/src/app/password-reset/password-reset.component.ts
+++ ClientDashboard_GUI/src/app/password-reset/password-reset.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-password-reset',
-  imports: [],
+  imports: [FormsModule, PasswordModule, IftaLabelModule, ButtonModule, RouterLink],
   templateUrl: './password-reset.component.html',
   styleUrl: './password-reset.component.css'
 })
 export class PasswordResetComponent {
+  newPassword: string = '';
+  confirmPassword: string = '';
 
+  resetPassword() {
+    //  password reset logic
+    if (this.newPassword === this.confirmPassword) {
+      console.log('Passwords match, proceed with reset');
+      // Call your service 
+    } else {
+      console.log('Passwords do not match');
+    }
+  }
 }
