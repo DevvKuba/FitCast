1c9abb5b801e6808c1a80d4303cb63b1af3900b8
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-31 13:00:04 +0000

    Conclude CreateAndSendPasswordResetEmailAsync functionality within PasswordResetService

--- ClientDashboard_API/Services/PasswordResetService.cs
+++ ClientDashboard_API/Services/PasswordResetService.cs
@@ -6,17 +6,26 @@ namespace ClientDashboard_API.Services
 {
     public class PasswordResetService(IUnitOfWork unitOfWork, IPasswordResetLinkFactory linkFactory, IFluentEmail fluentEmail) : IPasswordResetService
     {
-        public Task CreateAndSendPasswordResetEmailAsync(UserBase user)
+        public async Task CreateAndSendPasswordResetEmailAsync(UserBase user)
         {
-            //DateTime currentTime = DateTime.UtcNow;
-            //var passwordResetToken = new PasswordResetToken
-            //{
-            //    CreatedOnUtc = currentTime,
-            //    ExpiresOnUtc = currentTime.AddDays(1),
-            //    UserId = user.Id,
-            //}
+            DateTime currentTime = DateTime.UtcNow;
+            var passwordResetToken = new PasswordResetToken
+            {
+                CreatedOnUtc = currentTime,
+                ExpiresOnUtc = currentTime.AddDays(1),
+                UserId = user.Id
+            };
 
-            throw new NotImplementedException();
+            await unitOfWork.PasswordResetTokenRepository.AddPasswordResetTokenAsync(passwordResetToken);
+            await unitOfWork.Complete();
+
+            string resetRedirectionLink = linkFactory.Create(passwordResetToken);
+
+            await fluentEmail
+               .To(user.Email)
+               .Subject("Password Reset for FitCast")
+               .Body($"To reset your existing password <a href='{resetRedirectionLink}'>click here</a>", isHtml: true)
+               .SendAsync();
         }
     }
 }
