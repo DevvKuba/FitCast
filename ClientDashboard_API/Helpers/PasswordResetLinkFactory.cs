9eedff2b71ab7a2e8238d20c3ad0055b2f3217be
DevvKuba <jrfitnessinbox@gmail.com>
2025-12-31 12:43:32 +0000

    Provide development / production FrontendUrl's within configuration, using values to conclude PasswordResetLinkFactory Create method

--- ClientDashboard_API/Helpers/PasswordResetLinkFactory.cs
+++ ClientDashboard_API/Helpers/PasswordResetLinkFactory.cs
@@ -3,11 +3,16 @@ using ClientDashboard_API.Interfaces;
 
 namespace ClientDashboard_API.Helpers
 {
-    public class PasswordResetLinkFactory : IPasswordResetLinkFactory
+    public class PasswordResetLinkFactory(IConfiguration configuration) : IPasswordResetLinkFactory
     {
         public string Create(PasswordResetToken passwordResetToken)
         {
-            throw new NotImplementedException();
+            var frontendUrl = configuration["FrontendUrl"]
+                ?? throw new InvalidOperationException("FrontendUrl is not configured");
+
+            string resetRedirectionLink = $"{frontendUrl}/password-reset?token={passwordResetToken.Id}";
+
+            return resetRedirectionLink;
         }
     }
 }
