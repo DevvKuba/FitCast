899252098f5729a45df9584e8b9a31562ef961df
DevvKuba <jrfitnessinbox@gmail.com>
2026-01-03 19:43:43 +0000

    Update conditional checking for excluded name under trainer, within Hevy Workout collection process

--- ClientDashboard_API/Services/SessionSyncService.cs
+++ ClientDashboard_API/Services/SessionSyncService.cs
@@ -1,5 +1,6 @@
 ﻿using ClientDashboard_API.Entities;
 using ClientDashboard_API.Interfaces;
+using System.Diagnostics.Eventing.Reader;
 
 namespace ClientDashboard_API.Services
 {
@@ -10,54 +11,63 @@ namespace ClientDashboard_API.Services
         {
             // finds the trainer get object
             var dailyWorkouts = await hevyParser.CallApiForTrainerAsync(trainer);
-            int duplicateCount = 0;
+            int duplicateOrExcludedCount = 0;
             // look through workouts and do identical functionality compared to above methods
             // just calling different messageService methods NOT pipeline ones
 
             foreach (var workout in dailyWorkouts)
             {
                 string clientName = workout.Title.Split(' ')[0];
-                // this client name will always be retrived during syncing
-                var client = await unitOfWork.ClientRepository.GetClientByNameAsync(clientName);
 
-                if (await unitOfWork.ClientRepository.CheckIfClientExistsAsync(clientName) && !trainer.ExcludedNames.Contains(clientName))
+                if (!trainer.ExcludedNames.Contains(clientName.ToLower()))
                 {
+                    // this client name will always be retrived during syncing
+                    var client = await unitOfWork.ClientRepository.GetClientByNameAsync(clientName);
 
-                    var existingWorkout = await unitOfWork.WorkoutRepository.GetClientWorkoutAtDateByIdAsync(client!.Id, workout.SessionDate);
-                    // if workout is not a duplicate / not yet added
-                    if (existingWorkout == null)
+                    if (await unitOfWork.ClientRepository.CheckIfClientExistsAsync(clientName))
                     {
-                        unitOfWork.ClientRepository.UpdateAddingClientCurrentSessionAsync(client);
-                        await unitOfWork.WorkoutRepository.AddWorkoutAsync(client, workout.Title, workout.SessionDate, workout.ExerciseCount, workout.Duration);
-                        await unitOfWork.Complete();
 
-                        // indicating that their block is finished
-                        if (client.CurrentBlockSession == client.TotalBlockSessions)
+                        var existingWorkout = await unitOfWork.WorkoutRepository.GetClientWorkoutAtDateByIdAsync(client!.Id, workout.SessionDate);
+                        // if workout is not a duplicate / not yet added
+                        if (existingWorkout == null)
                         {
-                            await notificationService.SendTrainerReminderAsync(trainer.Id, client.Id);
-                            if (trainer.AutoPaymentSetting)
+                            unitOfWork.ClientRepository.UpdateAddingClientCurrentSessionAsync(client);
+                            await unitOfWork.WorkoutRepository.AddWorkoutAsync(client, workout.Title, workout.SessionDate, workout.ExerciseCount, workout.Duration);
+                            await unitOfWork.Complete();
+
+                            // indicating that their block is finished
+                            if (client.CurrentBlockSession == client.TotalBlockSessions)
                             {
-                                await autoPaymentService.CreatePendingPaymentAsync(trainer, client);
-                                await unitOfWork.Complete();
+                                await notificationService.SendTrainerReminderAsync(trainer.Id, client.Id);
+                                if (trainer.AutoPaymentSetting)
+                                {
+                                    await autoPaymentService.CreatePendingPaymentAsync(trainer, client);
+                                    await unitOfWork.Complete();
+                                }
                             }
                         }
+                        else
+                        {
+                            duplicateOrExcludedCount++;
+                        }
                     }
+
                     else
                     {
-                        duplicateCount++;
+                        var newClient = await unitOfWork.ClientRepository.AddNewClientUnderTrainerAsync(clientName, null, null, trainer.Id);
+                        await unitOfWork.Complete();
+
+                        unitOfWork.ClientRepository.UpdateAddingClientCurrentSessionAsync(newClient!);
+                        await unitOfWork.WorkoutRepository.AddWorkoutAsync(newClient!, workout.Title, workout.SessionDate, workout.ExerciseCount, workout.Duration);
+                        await unitOfWork.Complete();
                     }
                 }
                 else
                 {
-                    var newClient = await unitOfWork.ClientRepository.AddNewClientUnderTrainerAsync(clientName, null, null, trainer.Id);
-                    await unitOfWork.Complete();
-
-                    unitOfWork.ClientRepository.UpdateAddingClientCurrentSessionAsync(newClient!);
-                    await unitOfWork.WorkoutRepository.AddWorkoutAsync(newClient!, workout.Title, workout.SessionDate, workout.ExerciseCount, workout.Duration);
-                    await unitOfWork.Complete();
+                    duplicateOrExcludedCount++;
                 }
             }
-            return dailyWorkouts.Count() - duplicateCount;
+            return dailyWorkouts.Count() - duplicateOrExcludedCount;
         }
     }
 }
