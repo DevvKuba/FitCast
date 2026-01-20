5435ad374d9b0a78413c2bc88577169179962997
DevvKuba <jrfitnessinbox@gmail.com>
2026-01-20 21:45:16 +0000

    Create and apply migrations introducing NotificationsEnabled property

---
+++ ClientDashboard_API/Data/Migrations/20260120213532_AddNotificationsEnabledPropertyToUserBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientDashboard_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotificationsEnabledPropertyToUserBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NotificationsEnabled",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotificationsEnabled",
+                table: "Users");
+        }
+    }
+}
