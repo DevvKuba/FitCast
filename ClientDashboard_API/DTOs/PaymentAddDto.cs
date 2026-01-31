7250b42ad5a5cc1ba0225d91e6eaccdf7df86cda
DevvKuba <jrfitnessinbox@gmail.com>
2026-01-31 23:53:02 +0000

    Finalise data annotations for payment dtos

--- ClientDashboard_API/DTOs/PaymentAddDto.cs
+++ ClientDashboard_API/DTOs/PaymentAddDto.cs
@@ -1,4 +1,6 @@
-﻿namespace ClientDashboard_API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientDashboard_API.DTOs
 {
     public class PaymentAddDto
     {
@@ -6,13 +8,16 @@
 
         public required int ClientId { get; set; }
 
+        [Required(ErrorMessage = "Payment amount must be provided")]
         public required decimal Amount { get; set; }
 
-
+        [Required(ErrorMessage = "Payment sessions must be provided")]
         public required int NumberOfSessions { get; set; }
 
+        [Required(ErrorMessage = "Payment date must be provided")]
         public required string PaymentDate { get; set; }
 
+        [Required(ErrorMessage = "Payment status must be provided")]
         public required bool Confirmed { get; set; }
     }
 }
--- ClientDashboard_API/DTOs/PaymentUpdateRequestDto.cs
+++ ClientDashboard_API/DTOs/PaymentUpdateRequestDto.cs
@@ -1,17 +1,24 @@
-﻿namespace ClientDashboard_API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientDashboard_API.DTOs
 {
     public class PaymentUpdateRequestDto
     {
         public required int Id { get; set; }
 
+        [Required(ErrorMessage = "Payment amount must be provided")]
         public required decimal Amount { get; set; }
 
+        [Required(ErrorMessage = "Payment currency must be provided")]
         public required string Currency { get; set; }
 
+        [Required(ErrorMessage = "Payment sessions must be provided")]
         public required int NumberOfSessions { get; set; }
 
+        [Required(ErrorMessage = "Payment date must be provided")]
         public required string PaymentDate { get; set; }
 
+        [Required(ErrorMessage = "Payment status must be provided")]
         public required bool Confirmed { get; set; }
     }
 }
