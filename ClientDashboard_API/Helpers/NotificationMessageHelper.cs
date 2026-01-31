f62cb4b1aa207e5f32e352ad50dc432c637ab153
DevvKuba <jrfitnessinbox@gmail.com>
2026-01-31 19:29:13 +0000

    Deliver NotificationMessageHelper class, containing dictionary that allows for custom retrieval of notifications messages with embedded values

---
+++ ClientDashboard_API/Helpers/NotificationMessageHelper.cs
@@ -0,0 +1,29 @@
+﻿using ClientDashboard_API.Entities;
+
+namespace ClientDashboard_API.Helpers
+{
+    public static class NotificationMessageHelper
+    {
+        private static readonly Dictionary<Enums.NotificationType, Func<Trainer, Client, string>> MessageTemplates = new()
+        {
+            [Enums.NotificationType.TrainerBlockCompletionReminder] =
+            (client, trainer) => $"{client.FirstName}'s monthly sessions have come to an end,\n" +
+                                    $"remember to message them in regards of a new monthly payment.",
+            [Enums.NotificationType.ClientBlockCompletionReminder] =
+            (client, trainer) => $"Hey {client.FirstName}! just wanted to" +
+                                    "inform you that our monthly sessions have come to an end,\n" +
+                    $"If you could place a block payment before our next session block that would be great."
+        };
+
+        public static string GetMessage (Enums.NotificationType notificationType, Trainer trainer, Client client)
+        {
+            if (MessageTemplates.TryGetValue(notificationType, out var notificationMessage))
+            {
+                return notificationMessage(trainer, client);
+            }
+
+            throw new ArgumentException($"No message template found for notification type: {notificationType}");
+        }
+        
+    }
+}
