cf7b37277401618bb6011bba50ff9dc6383887d3
DevvKuba <jrfitnessinbox@gmail.com>
2026-02-01 23:07:26 +0000

    Set up data annotations within LoginDto

--- ClientDashboard_API/DTOs/LoginDto.cs
+++ ClientDashboard_API/DTOs/LoginDto.cs
@@ -1,13 +1,17 @@
 ﻿using ClientDashboard_API.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ClientDashboard_API.DTOs
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "Email must be provided")]
         public required string Email { get; set; }
 
+        [Required(ErrorMessage = "Password must be provided")]
         public required string Password { get; set; }
 
+        [Required(ErrorMessage = "Role must be selected")]
         public required UserRole Role { get; set; }
     }
 }
