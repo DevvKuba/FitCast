c3fb5cfbec161f5eba1135f8356ffbebf24241d9
DevvKuba <jrfitnessinbox@gmail.com>
2026-02-11 15:16:35 +0000

    Provide training methods within IMLModelTrainingService interface

--- ClientDashboard_API/ML/Interfaces/IMLModelTrainingService.cs
+++ ClientDashboard_API/ML/Interfaces/IMLModelTrainingService.cs
@@ -1,6 +1,14 @@
-﻿namespace ClientDashboard_API.ML.Interfaces
+﻿using ClientDashboard_API.ML.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace ClientDashboard_API.ML.Interfaces
 {
     public interface IMLModelTrainingService
     {
+        // trainers a model for a specific trainer
+        Task<ModelMetrics> TrainModelAsync(int trainerId);
+
+        // trainers models for all trainers with sufficient data
+        Task<Dictionary<int, ModelMetrics>> TrainAllModelsAsync();
     }
 }
