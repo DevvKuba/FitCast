2296d3ad79b7eb0de3c86f6b6953c428172ac2c9
DevvKuba <jrfitnessinbox@gmail.com>
2026-02-11 13:47:54 +0000

    Declare all properties for ML training model within TrainerRevenueData

--- ClientDashboard_API/ML/Models/TrainerRevenueData.cs
+++ ClientDashboard_API/ML/Models/TrainerRevenueData.cs
@@ -1,6 +1,41 @@
-﻿namespace ClientDashboard_API.ML.Models
+﻿using Microsoft.ML.Data;
+
+namespace ClientDashboard_API.ML.Models
 {
     public class TrainerRevenueData
     {
+        // input features
+
+        [LoadColumn(0)]
+        public float ActiveClients { get; set; }
+
+        [LoadColumn(1)]
+        public float TotalSessionThisMonth { get; set; }
+
+        [LoadColumn(2)]
+        public float AverageSessionPrice { get; set; }
+
+        [LoadColumn(3)]
+        public float NewClientsThisMonth { get; set; }
+
+        [LoadColumn(4)]
+        public float MonthlyRevenueThusFar { get; set; }
+
+        // engineered features
+
+        [LoadColumn(5)]
+        public float SessionsPerClient { get; set; }
+
+        [LoadColumn(6)]
+        public float DayOfMonth { get; set; }
+
+        [LoadColumn(7)]
+        public float GrowthRate { get; set; }
+
+        // what we're predicting
+
+        [LoadColumn(8)]
+        [ColumnName("Label")]
+        public float NextMonthRevenue { get; set; }
     }
 }
