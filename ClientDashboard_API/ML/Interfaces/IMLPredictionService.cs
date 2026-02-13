7154b5159be747b67430f98ee24ba8e00c4f7869
DevvKuba <jrfitnessinbox@gmail.com>
2026-02-13 11:28:40 +0000

    Instantiase IMLPredictionService interface and implement within TrainerRevenueMLPredictionService class

--- ClientDashboard_API/ML/Interfaces/IMLPredictionService.cs
+++ ClientDashboard_API/ML/Interfaces/IMLPredictionService.cs
@@ -1,6 +1,13 @@
-﻿namespace ClientDashboard_API.ML.Interfaces
+﻿using ClientDashboard_API.ML.Models;
+
+namespace ClientDashboard_API.ML.Interfaces
 {
     public interface IMLPredictionService
     {
+        // Predicts next month's revenue for a trainer based on current month data
+        Task<float> PredictNextMonthRevenueAsync(int trainerId);
+
+        // Gets predictions for all trainers
+        Task<Dictionary<int, float>> PredictForAllTrainersAsync();
     }
 }
--- ClientDashboard_API/ML/Services/TrainerRevenueMLPredictionService.cs
+++ ClientDashboard_API/ML/Services/TrainerRevenueMLPredictionService.cs
@@ -1,6 +1,17 @@
-﻿namespace ClientDashboard_API.ML.Services
+﻿using ClientDashboard_API.ML.Interfaces;
+
+namespace ClientDashboard_API.ML.Services
 {
-    public class TrainerRevenueMLPredictionService
+    public class TrainerRevenueMLPredictionService : IMLPredictionService
     {
+        public Task<Dictionary<int, float>> PredictForAllTrainersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<float> PredictNextMonthRevenueAsync(int trainerId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
