e1b5f1403768251ece2b256addbd590f9263e02f
DevvKuba <jrfitnessinbox@gmail.com>
2026-02-13 12:07:32 +0000

    Prepare both prediction value output and training http controller calls within MLPredictionController

--- ClientDashboard_API/Controllers/MLPredictionController.cs
+++ ClientDashboard_API/Controllers/MLPredictionController.cs
@@ -1,6 +1,25 @@
-﻿namespace ClientDashboard_API.Controllers
+﻿using ClientDashboard_API.DTOs;
+using ClientDashboard_API.Enums;
+using ClientDashboard_API.ML.Interfaces;
+using ClientDashboard_API.ML.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientDashboard_API.Controllers
 {
-    public class MLPredictionController
+    [Authorize(Roles = "Trainer")]
+    public class MLPredictionController(IMLPredictionService predictionService, IMLModelTrainingService trainingService) : BaseAPIController
     {
+        [HttpGet("predictMyRevenue")]
+        public async Task<ActionResult<ApiResponseDto<PredictionResultDto>>> PredictMyRevenueAsync([FromQuery] int trainerId)
+        {
+
+        }
+
+        [HttpPost("trainRevenueModel")]
+        public async Task<ActionResult<ApiResponseDto<ModelMetrics>>> TrainRevenueModelAsync([FromQuery] int trainerId)
+        {
+
+        }
     }
 }
---
+++ ClientDashboard_API/DTOs/PredictionResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ClientDashboard_API.DTOs
+{
+    public class PredictionResultDto
+    {
+        public int TrainerId { get; set; }
+
+        public float PredictedRevenue { get; set; }
+
+        public DateTime PredictedDate { get; set; }
+    }
+}
