d5bebd60a7c4ffc7b493d1dc24a9d8bdcb5a410e
DevvKuba <jrfitnessinbox@gmail.com>
2026-03-13 13:54:21 +0000

    Complete UpdateTrainersPaymentVisibilityStatussesAsync payment controller call, utilising new repository method

--- ClientDashboard_API/Controllers/PaymentController.cs
+++ ClientDashboard_API/Controllers/PaymentController.cs
@@ -60,8 +60,26 @@ namespace ClientDashboard_API.Controllers
             }
             // we are returning payment within trainer or client
             return Ok(new ApiResponseDto<string> { Data = payment.Id.ToString(), Message = $"Payment for client: {payment.Client!.FirstName} has been updated successfully", Success = true });
+        }
 
+        [Authorize(Roles = "Trainer")]
+        [HttpPut("updateAllPaymentVisibilityStatusses")]
+        public async Task<ActionResult<ApiResponseDto<string>>> UpdateTrainersPaymentVisibilityStatussesAsync([FromQuery] int trainerId)
+        {
+            var trainer = await unitOfWork.TrainerRepository.GetTrainerByIdAsync(trainerId);
 
+            if (trainer is null)
+            {
+                return NotFound(new ApiResponseDto<string> { Data = null, Message = $"trainer does not exist", Success = false });
+            }
+
+            await unitOfWork.PaymentRepository.UpdateAllTrainerPaymentsToVisibleStatusAsync(trainer);
+
+            if (!await unitOfWork.Complete())
+            {
+                return BadRequest(new ApiResponseDto<string> { Data = null, Message = "error saving payment statusses", Success = false });
+            }
+            return Ok(new ApiResponseDto<string> { Data = null, Message = $"Updated all payment to visible, for trainer with id: {trainerId}", Success = true });
         }
 
         [Authorize(Roles = "Trainer")]
