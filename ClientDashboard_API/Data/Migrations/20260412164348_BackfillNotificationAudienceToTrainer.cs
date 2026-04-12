08e68ccb9de47ded1e059008a286ae6bdf3dbc77
DevvKuba <jrfitnessinbox@gmail.com>
2026-04-12 17:45:16 +0100

    Set up SQL script within migration ensuring all current notifications are set to Trainer entity type

---
+++ ClientDashboard_API/Data/Migrations/20260412164348_BackfillNotificationAudienceToTrainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientDashboard_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillNotificationAudienceToTrainer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE [Notifications] SET [Audience] = 1;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
