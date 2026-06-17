a0d1b39bda9d6e28ac672fefae8999b47dbfa816
DevvKuba <jrfitnessinbox@gmail.com>
2026-06-17 23:17:43 +0100

    Reformat to utilise a primary constructor in TrainerRevenueMLPredictionService, along with the introduction of the BlobServiceClient used in the new LoadBlobStorageModelForTrainer method call

--- ClientDashboard_API/ML/Services/TrainerRevenueMLPredictionService.cs
+++ ClientDashboard_API/ML/Services/TrainerRevenueMLPredictionService.cs
@@ -1,4 +1,5 @@
-﻿using ClientDashboard_API.Interfaces;
+﻿using Azure.Storage.Blobs;
+using ClientDashboard_API.Interfaces;
 using ClientDashboard_API.ML.Helpers;
 using ClientDashboard_API.ML.Interfaces;
 using ClientDashboard_API.ML.Models;
@@ -6,40 +7,30 @@ using Microsoft.ML;
 
 namespace ClientDashboard_API.ML.Services
 {
-    public class TrainerRevenueMLPredictionService : IMLPredictionService
+    public class TrainerRevenueMLPredictionService(
+        IUnitOfWork unitOfWork,
+        ILogger<TrainerRevenueMLPredictionService> logger,
+        IWebHostEnvironment environment) : IMLPredictionService
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly ILogger<TrainerRevenueMLPredictionService> _logger;
-        private readonly MLContext _mlContext;
-        private readonly string _modelsPath;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly ILogger<TrainerRevenueMLPredictionService> _logger = logger;
+        private readonly MLContext _mlContext = new MLContext();
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _modelsPath = Path.Combine(environment.ContentRootPath, "ML", "TrainedModels");
 
         // Cache loaded models in memory (avoid loading from disk every time)
-        private readonly Dictionary<int, PredictionEngine<TrainerRevenueData, TrainerRevenuePrediction>> _predictionEngines;
-
-        public TrainerRevenueMLPredictionService(
-            IUnitOfWork unitOfWork,
-            ILogger<TrainerRevenueMLPredictionService> logger,
-            IWebHostEnvironment environment)
-        {
-            _unitOfWork = unitOfWork;
-            _logger = logger;
-            _mlContext = new MLContext();
-            _modelsPath = Path.Combine(environment.ContentRootPath, "ML", "TrainedModels");
-            _predictionEngines = new Dictionary<int, PredictionEngine<TrainerRevenueData, TrainerRevenuePrediction>>();
-        }
+        private readonly Dictionary<int, PredictionEngine<TrainerRevenueData, TrainerRevenuePrediction>> _predictionEngines = new Dictionary<int, PredictionEngine<TrainerRevenueData, TrainerRevenuePrediction>>();
 
         public async Task<float> PredictNextMonthRevenueAsync(int trainerId)
         {
-            // 1 Ensure latest model from disk is used after retraining
             if(_predictionEngines.ContainsKey(trainerId))
             {
                 _predictionEngines.Remove(trainerId);
             }
 
-            LoadModelForTrainer(trainerId);
+            LoadLocalModelForTrainer(trainerId);
             var predictionEngine = _predictionEngines[trainerId];
 
-            // 2 fetch latest data for trainer
             var lastRecord = await _unitOfWork.TrainerDailyRevenueRepository.GetLatestRevenueRecordForTrainerAsync(trainerId);
 
             if(lastRecord is null)
@@ -47,10 +38,8 @@ namespace ClientDashboard_API.ML.Services
                 throw new FileNotFoundException($"no daily revenue records found for Trainer: {trainerId}");
             }
 
-            // 3 prepare prediction input
             var inputData = FeatureEngineeringHelper.PreparePredictionData(lastRecord);
 
-            // 4 make prediction
             var prediction = predictionEngine.Predict(inputData);
 
             _logger.LogInformation(
@@ -60,6 +49,7 @@ namespace ClientDashboard_API.ML.Services
             return (float)Math.Round(prediction.PredictedRevenue, 0);
         }
 
+        // remove and associated test cases
         public async Task<Dictionary<int, float>> PredictForAllTrainersAsync()
         {
             var trainers = await _unitOfWork.TrainerRepository.GetAllTrainersAsync();
@@ -81,7 +71,7 @@ namespace ClientDashboard_API.ML.Services
             return predictions;
         }
 
-        private void LoadModelForTrainer(int trainerId)
+        private void LoadLocalModelForTrainer(int trainerId)
         {
             var modelPath = Path.Combine(_modelsPath, $"trainer_{trainerId}_revenue_model.zip");
             
@@ -102,6 +92,28 @@ namespace ClientDashboard_API.ML.Services
             _predictionEngines[trainerId] = predictionEngine;
 
             _logger.LogInformation("Loaded model for Trainer {TrainerId} from {Path}", trainerId, modelPath);
+        }
+
+
+        private async Task LoadBlobStorageModelForTrainer(int trainerId)
+        {
+            var container = _blobServiceClient.GetBlobContainerClient("ml-models");
+
+            var blob = container.GetBlobClient($"trainer_{trainerId}_revenue_model.zip");
+
+            if(!await blob.ExistsAsync())
+            {
+                throw new FileNotFoundException($"No trainer model was retrieved for trainer with id: {trainerId}");
+            }
+
+            using var stream = new MemoryStream();
+            await blob.DownloadToAsync(stream);
+            stream.Position = 0;
+
+            var model = _mlContext.Model.Load(stream, out var schema);
+
+            var predictionEngine = _mlContext.Model
+               .CreatePredictionEngine<TrainerRevenueData, TrainerRevenuePrediction>(model);
         }
 
     }
