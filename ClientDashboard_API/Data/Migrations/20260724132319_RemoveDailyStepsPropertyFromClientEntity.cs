ab35dd8f2fad302f791433d27fd8a1f9e97b7509
DevvKuba <jrfitnessinbox@gmail.com>
2026-07-24 14:24:27 +0100

    Create migration for recent property removal along with updating database

---
+++ ClientDashboard_API/Data/Migrations/20260724132319_RemoveDailyStepsPropertyFromClientEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientDashboard_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDailyStepsPropertyFromClientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailySteps",
+                table: "Clients");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DailySteps",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
