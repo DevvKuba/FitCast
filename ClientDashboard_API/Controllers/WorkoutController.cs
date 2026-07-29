3a09a710c9aa2fc8172b3e0be557a89eb08d93c7
DevvKuba <jrfitnessinbox@gmail.com>
2026-07-29 21:50:14 +0100

    Delete unused controller calls as part of workout controller class

--- ClientDashboard_API/Controllers/WorkoutController.cs
+++ ClientDashboard_API/Controllers/WorkoutController.cs
@@ -59,75 +59,6 @@ namespace ClientDashboard_API.Controllers
         }
 
         /// <summary>
-        /// Workout request for the retrieval of all daily workouts
-        /// </summary>
-        [Authorize(Roles = "Trainer")]
-        [HttpGet("GetAllDailySessions")]
-        public async Task<ActionResult<ApiResponseDto<List<Workout>>>> GetAllDailyClientWorkoutsAsync()
-        {
-            var todaysDateString = DateTime.Now.Date.ToString();
-            var todaysDate = DateOnly.Parse(todaysDateString[0..10]);
-
-            var clientSessions = await unitOfWork.WorkoutRepository.GetClientWorkoutsAtDateAsync(todaysDate);
-
-            if (clientSessions is null || !clientSessions.Any())
-            {
-                return NotFound(new ApiResponseDto<List<Workout>> { Data = [], Message = $"No client sessions found on specified date: {todaysDateString}", Success = false });
-            }
-
-            return Ok(new ApiResponseDto<List<Workout>> { Data = clientSessions, Message = "Daily workouts retrieved successfully", Success = true });
-        }
-
-        /// <summary>
-        /// Workout request for retrieving a specific client workout, at a given date
-        /// </summary>
-        [Authorize(Roles = "Trainer")]
-        [HttpGet("{clientName}/{workoutDate}/GetWorkoutAtDate")]
-        public async Task<ActionResult<ApiResponseDto<Workout>>> GetClientWorkoutAtDateAsync(string clientName, DateOnly workoutDate)
-        {
-            var clientWorkout = await unitOfWork.WorkoutRepository.GetClientWorkoutAtDateByNameAsync(clientName, workoutDate);
-            if (clientWorkout is null)
-            {
-                return NotFound(new ApiResponseDto<Workout> { Data = null, Message = $"Client: {clientName}'s workout at {workoutDate} was not found.", Success = false });
-            }
-
-            return Ok(new ApiResponseDto<Workout> { Data = clientWorkout, Message = "Workout retrieved successfully", Success = true });
-        }
-
-        /// <summary>
-        /// Workout request for retrieving a list of client workouts,
-        /// from a given date
-        /// </summary>
-        [Authorize(Roles = "Trainer")]
-        [HttpGet("{workoutDate}/GetWorkoutsFromDate")]
-        public async Task<ActionResult<ApiResponseDto<List<Workout>>>> GetClientWorkoutsFromDateAsync(DateOnly workoutDate)
-        {
-            var clientWorkouts = await unitOfWork.WorkoutRepository.GetClientWorkoutsFromDateAsync(workoutDate);
-            if (!clientWorkouts.Any())
-            {
-                return NotFound(new ApiResponseDto<List<Workout>> { Data = [], Message = $"No client sessions found on specified date: {workoutDate}", Success = false });
-            }
-
-            return Ok(new ApiResponseDto<List<Workout>> { Data = clientWorkouts, Message = "Workouts from date retrieved successfully", Success = true });
-        }
-
-        /// <summary>
-        /// Workout request for the retrieval of a specific client's last workout
-        /// </summary>
-        [Authorize(Roles = "Trainer")]
-        [HttpGet("{clientName}/lastDate")]
-        public async Task<ActionResult<ApiResponseDto<Workout>>> GetLatestClientWorkoutAsync(string clientName)
-        {
-            var latestWorkoutInfo = await unitOfWork.WorkoutRepository.GetLatestClientWorkoutAsync(clientName);
-            if (latestWorkoutInfo is null)
-            {
-                return NotFound(new ApiResponseDto<Workout> { Data = null, Message = $"{clientName} has no workouts recorded", Success = false });
-            }
-
-            return Ok(new ApiResponseDto<Workout> { Data = latestWorkoutInfo, Message = $"Latest workout for {clientName} retrieved successfully", Success = true });
-        }
-
-        /// <summary>
         /// Workout request for adding a workout for a specific client, utilised within SessionSyncService
         /// </summary>
         [Authorize(Roles = "Trainer")]
@@ -247,30 +178,6 @@ namespace ClientDashboard_API.Controllers
             }
             return Ok(new ApiResponseDto<string> { Data = workout.ClientName, Message = $"Workout with title: {workout.WorkoutTitle} at {workout.SessionDate} successfully updated", Success = true });
 
-
-        }
-
-        /// <summary>
-        /// Workout request for removing a specific workout via client name & date
-        /// </summary>
-        [Authorize(Roles = "Trainer")]
-        [HttpDelete("{clientName}/{sessionDate}")]
-        public async Task<ActionResult<ApiResponseDto<string>>> DeleteClientWorkoutAsync(string clientName, DateOnly workoutDate)
-        {
-            var clientWorkout = await unitOfWork.WorkoutRepository.GetClientWorkoutAtDateByNameAsync(clientName, workoutDate);
-            if (clientWorkout is null)
-            {
-                return NotFound(new ApiResponseDto<string> { Data = null, Message = $"Cannot find specified client: {clientName}'s workout at {workoutDate}", Success = false });
-            }
-            
-            unitOfWork.ClientRepository.UpdateDeletingClientCurrentSession(clientWorkout.Client!);
-            unitOfWork.WorkoutRepository.RemoveWorkout(clientWorkout);
-
-            if (!await unitOfWork.Complete())
-            {
-                return BadRequest(new ApiResponseDto<string> { Data = null, Message = "Removing client was unsuccessful", Success = false });
-            }
-            return Ok(new ApiResponseDto<string> { Data = clientName, Message = $"{clientName}'s workout at {workoutDate} has been removed", Success = true });
 
         }
 
