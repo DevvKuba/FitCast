dc3f94b400ee9e21fda77c758459b7662b432c47
DevvKuba <jrfitnessinbox@gmail.com>
2026-08-04 14:49:30 +0100

    Configure remaining Handlers for both the payment and workout entities

---
+++ ClientDashboard_API/Authorization/PaymentOwnershipHandler.cs
@@ -0,0 +1,26 @@
+﻿using ClientDashboard_API.Entities;
+using ClientDashboard_API.Interfaces.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Resources;
+
+namespace ClientDashboard_API.Authorization
+{
+    public class PaymentOwnershipHandler : AuthorizationHandler<ResourceOwnerRequirement, Payment>
+    {
+        protected override Task HandleRequirementAsync
+            (AuthorizationHandlerContext context, ResourceOwnerRequirement requirement, Payment resource)
+        {
+            var sub = context.User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            if (sub is null || !int.TryParse(sub, out var callerId))
+                return Task.CompletedTask;
+            
+            var isOwningTrainer = context.User.IsInRole("Trainer") && resource.TrainerId == callerId;
+
+            if (isOwningTrainer)
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ClientDashboard_API/Authorization/WorkoutOwnershipHandler.cs
@@ -0,0 +1,24 @@
+﻿using ClientDashboard_API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ClientDashboard_API.Authorization
+{
+    public class WorkoutOwnershipHandler : AuthorizationHandler<ResourceOwnerRequirement, Workout>
+    {
+        protected override Task HandleRequirementAsync
+            (AuthorizationHandlerContext context, ResourceOwnerRequirement requirement, Workout resource)
+        {
+            var sub = context.User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            if (sub is null || !int.TryParse(sub, out var callerId))
+                return Task.CompletedTask;
+
+            var isOwningTrainer = context.User.IsInRole("Trainer") && resource.Client!.TrainerId == callerId;
+
+            if (isOwningTrainer)
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
